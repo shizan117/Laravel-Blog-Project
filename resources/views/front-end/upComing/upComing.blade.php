8d8641a28d935e286f3f0016eab6d8c4337370e8
shizan <shizan117@gmail.com>
2023-10-10 21:38:49 +0600

    day-1

---
+++ resources/views/front-end/upComing/upComing.blade.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en" class="minimal-theme">
+
+
+<!-- Mirrored from codervent.com/skodash/demo/tabular-menu/ltr/pages-errors-coming-soon.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 May 2022 06:15:48 GMT -->
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{asset('front-end-assets')}}/assets-1/images/favicon-32x32.png" type="image/png" />
+    <!--plugins-->
+    <link href="{{asset('front-end-assets')}}/assets-1/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+    <link href="{{asset('front-end-assets')}}/assets-1/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('front-end-assets')}}/assets-1/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="{{asset('front-end-assets')}}/assets-1/css/bootstrap-extended.css" rel="stylesheet" />
+    <link href="{{asset('front-end-assets')}}/assets-1/css/style.css" rel="stylesheet" />
+    <link href="{{asset('front-end-assets')}}/assets-1/css/icons.css" rel="stylesheet">
+{{--    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">--}}
+{{--    <link rel="stylesheet" href="../../../../../cdn.jsdelivr.net/npm/bootstrap-icons%401.5.0/font/bootstrap-icons.css">--}}
+
+{{--    <!-- loader-->--}}
+{{--    <link href="{{asset('front-end-assets')}}/assets-1/css/pace.min.css" rel="stylesheet" />--}}
+
+
+    <!--Theme Styles-->
+    <link href="{{asset('front-end-assets')}}/assets-1/css/dark-theme.css" rel="stylesheet" />
+    <link href="{{asset('front-end-assets')}}/assets-1/css/light-theme.css" rel="stylesheet" />
+    <link href="{{asset('front-end-assets')}}/assets-1/css/semi-dark.css" rel="stylesheet" />
+    <link href="{{asset('front-end-assets')}}/assets-1/css/header-colors.css" rel="stylesheet" />
+
+    <title>Skodash - Bootstrap 5 Admin Template</title>
+</head>
+
+<body>
+
+
+<!--start wrapper-->
+<div class="wrapper">
+
+    <main class="page-content-1">
+
+        <div class="row">
+            <div class="col-12 col-lg-6 mx-auto">
+                <div class="card shadow-none bg-transparent">
+                    <div class="card-body text-center">
+                        <h1 class="display-4 mt-5">We are Coming Soon!</h1>
+                        <p>We are currently working hard on this page. Subscribe our newsletter
+                            <br>to get update when it'll be live.</p>
+                        <div class="row">
+                            <div class="col-12 col-lg-12 mx-auto">
+                                <form class="">
+                                    <div class="input-group input-group-lg">
+                                        <input type="text" class="form-control" placeholder="Enter Your Email ID">
+                                        <button class="btn btn-primary" type="button"><i class="bx bx-right-arrow-alt"></i>
+                                        </button>
+                                    </div>
+                                </form>
+                                <h4 class="mt-3">Follow Us :</h4>
+                                <div class="error-social mt-3"> <a href="javascript:;" class="bg-facebook"><i class="bx bxl-facebook"></i></a>
+                                    <a href="javascript:;" class="bg-twitter"><i class="bx bxl-twitter"></i></a>
+                                    <a href="javascript:;" class="bg-google"><i class="bx bxl-google"></i></a>
+                                    <a href="javascript:;" class="bg-linkedin"><i class="bx bxl-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+    <!--end page main-->
+
+
+    <!--start overlay-->
+    <div class="overlay nav-toggle-icon"></div>
+    <!--end overlay-->
+
+    <!--Start Back To Top Button-->
+    <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
+    <!--End Back To Top Button-->
+
+    <!--start switcher-->
+    <div class="switcher-body">
+        <button class="btn btn-primary btn-switcher shadow-sm" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="bi bi-paint-bucket me-0"><-</i></button>
+        <div class="offcanvas offcanvas-end shadow border-start-0 p-2" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling">
+            <div class="offcanvas-header border-bottom">
+                <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Theme Customizer</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"></button>
+            </div>
+            <div class="offcanvas-body">
+                <h6 class="mb-0">Theme Variation</h6>
+                <hr>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="LightTheme" value="option1">
+                    <label class="form-check-label" for="LightTheme">Light</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="DarkTheme" value="option2">
+                    <label class="form-check-label" for="DarkTheme">Dark</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="SemiDarkTheme" value="option3">
+                    <label class="form-check-label" for="SemiDarkTheme">Semi Dark</label>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!--end switcher-->
+
+</div>
+<!--end wrapper-->
+
+
+<!-- Bootstrap bundle JS -->
+<script src="{{asset('front-end-assets')}}/assets-1/js/bootstrap.bundle.min.js"></script>
+<!--plugins-->
+<script src="{{asset('front-end-assets')}}/assets-1/js/jquery.min.js"></script>
+<script src="{{asset('front-end-assets')}}/assets-1/plugins/simplebar/js/simplebar.min.js"></script>
+<script src="{{asset('front-end-assets')}}/assets-1/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+<script src="{{asset('front-end-assets')}}/assets-1/js/pace.min.js"></script>
+<!--app-->
+<script src="{{asset('front-end-assets')}}/assets-1/js/app.js"></script>
+
+</body>
+
+
+<!-- Mirrored from codervent.com/skodash/demo/tabular-menu/ltr/pages-errors-coming-soon.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 May 2022 06:15:48 GMT -->
+</html>
